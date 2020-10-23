38f9f04e396c8c0f3adebc6175f3519b9d5a48e5
Bharathi Sakthivel <42297176+bharathi1294@users.noreply.github.com>
2020-10-23 16:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask,request,render_template
+from flask_cors import cross_origin
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
+@cross_origin()
 def home():
 	page=requests.get("https://api.quotable.io/random")
 	js=page.json()
@@ -14,4 +16,4 @@ def home():
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='127.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
